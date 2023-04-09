73c4d805528d0a64bd99d943a7d016d7717b283a
prnmshr <112613047+prnmshr@users.noreply.github.com>
2023-04-09 08:56:03 +0700

    Update latihan1.php

--- json/latihan1.php
+++ json/latihan1.php
@@ -4,17 +4,18 @@
 //     [
 //     "nama" => "Purnama Ashari",
 //     "nim" => "H1101211065",
-//     "email" => "h1101211065@student.untan.ac.id"
+//     "email" => "iniemail@student.untan.ac.id"
 //     ],
 
 //     [
-//         "nama" => "Nurdila",
-//         "nim" => "H1101211041",
-//         "email" => "h1101211065@student.untan.ac.id"
+//         "nama" => "Sie Exx",
+//         "nim" => "H1101211066",
+//         "email" => "inijugaemail@student.untan.ac.id"
 //     ]
 // ];
 
 //var_dump($mahasiswa);
+
 // ganti komentar untuk menjalankan program di atas
 //hubungkan ke database localhost anda untuk menjalankan program di bawah
 
